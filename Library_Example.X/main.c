cc2e04ee8cc5cc3c4594c146dd8221cfe7211693
Alejandro Reyes Leon <alle262001@gmail.com>
2023-07-13 23:19:18 -0500

    Update comments of this project.

--- Library_Example.X/main.c
+++ Library_Example.X/main.c
@@ -5,6 +5,16 @@
  * Created on July 12, 2023, 2:08 PM
  */
 
+/*
+ *   PROJECT DESCRIPTION : 
+ * 
+ * 
+ *      This is a simple exercise of microcontroller for use simple library
+ *      created by myself.  
+ * 
+ * 
+ */
+
 
 #include <xc.h> //Library to all microchip uControllers. 
 #include <pic18f4550.h> //Library to specific registers of PIC18F4550.
@@ -18,9 +28,10 @@ void Settings();
 
 void main(void) {
     Settings(); //Call function "Settings."
-    while (1) {
+    while (1) { //Infinity loop. 
 
-        RA0 ^= 1;
+        //This two statements blink the pin RA0 from A port. 
+        RA0 ^= 1; 
         __delay_ms(500);
 
     }
@@ -30,9 +41,9 @@ void main(void) {
 
 void Settings() {
 
-    OSCCON = 0x72;
-    ADCON1 = 0x0F;
-    TRISAbits.RA0 = 0;
-    LATAbits.LA0 = 0;
+    OSCCON = 0x72;  //Set register "Oscillator Control"
+    ADCON1 = 0x0F;  //All pins as a digital. 
+    TRISAbits.RA0 = 0; //Set pin as an output.
+    LATAbits.LA0 = 0; //Set pin low logic state. 
 
 }
