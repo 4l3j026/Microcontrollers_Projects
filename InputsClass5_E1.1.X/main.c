fc4c2cfc92fb4ff3bb0770ffb6e0c64f004dfa64
Daniel ALejandro Reyes León <alle262001@gmail.com>
2023-08-21 21:39:30 -0500

    Adding projecto description.

--- InputsClass5_E1.1.X/main.c
+++ InputsClass5_E1.1.X/main.c
@@ -5,6 +5,13 @@
  * Created on August 21, 2023, 5:50 PM
  */
 
+/*
+        Project Description: 
+ *          This project show the use of digital inputs reading the pin with 
+ *          PORT register. 
+ *          So with two buttons turn on two leds, sets as pull-up and pull down. 
+ */
+
 //Adding Libraries 
 #include <xc.h> //Library to microchip microcontrollers. 
 #include "Fuses.h" //Personal Library to se fuses. 
@@ -15,11 +22,16 @@ void Configurations (void);
 
 
 
-
+//Main function. 
 void main(void) {
     
     //Call functions.
     Configurations();
+    
+    //Infinite loop
+    while(1){
+        
+    }
     
     
 }
@@ -33,5 +45,9 @@ void Configurations (){
     //Set pin RB2 & RB3 as output.
     TRISBbits.RB2 = 0; 
     TRISBbits.RB3 = 0; 
+    
+    //Set RB2 & RB3 as low logic state. 
+    LATBbits.LB2 = 0;
+    LATBbits.LB3 = 0; 
     
 }
