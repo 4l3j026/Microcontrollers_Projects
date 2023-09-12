efb57c6ab3f6e70de6dce0fa0d5ad21dc7ace804
Daniel ALejandro Reyes León <alle262001@gmail.com>
2023-09-12 18:53:54 -0500

    Create project of binary counter.

---
+++ InputsClass5_E1.4.X/main.c
@@ -0,0 +1,41 @@
+/*
+ * File:   main.c
+ * Author: Alejo
+ *
+ * Created on September 12, 2023, 6:40 PM
+ */
+
+
+#include <xc.h> //Library to Microchip microcontrollers. 
+#include "Fuses.h" //Library to set fuses of PIC. 
+
+//Prototype functions. 
+void Conf_Reg(void);
+
+//Main function
+
+void main(void) {
+
+    while (1) {
+
+    }
+
+}
+
+//Developing function. 
+
+void Conf_Reg(void) {
+
+    OSCCON = 0x72; //Set internal oscillator. 
+    ADCON1 = 0x0F; //Set ports as digital. 
+
+    //
+//    TRISAbits.RA0 = 1;
+//    TRISAbits.RA1 = 1;
+    TRISA = 0x03; 
+    
+
+}
+
+
+
