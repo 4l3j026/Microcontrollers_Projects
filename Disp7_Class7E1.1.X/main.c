1f80fdf59819bfd3e3051801b5a4445c514b0c06
Daniel ALejandro Reyes León <alle262001@gmail.com>
2023-10-11 09:33:04 -0500

    Create new project to manage 7- segment display.

---
+++ Disp7_Class7E1.1.X/main.c
@@ -0,0 +1,22 @@
+/*
+ * File:   main.c
+ * Author: Alejo
+ *
+ * Created on October 11, 2023, 9:27 AM
+ * 
+ *      Project Description: 
+ * 
+ *          Using Display 7 
+ */
+
+
+#include <xc.h>
+#include "Fuses.h"
+
+void main(void) {
+
+    while (1) {
+
+    }
+
+}
