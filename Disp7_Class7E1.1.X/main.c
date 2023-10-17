b8d310ce56922bcce2756c3187d79673e1e2bdf8
Alejandro Reyes Leon <alle262001@gmail.com>
2023-10-16 21:29:29 -0500

    Create fuses library and update files of the project.

--- Disp7_Class7E1.1.X/main.c
+++ Disp7_Class7E1.1.X/main.c
@@ -6,12 +6,14 @@
  * 
  *      Project Description: 
  * 
- *          Using Display 7 
+ *          Using Display 7-Segments to show a counter from 0 to 9 and using interruptions (button falling edge). 
+ *          
  */
 
 
-#include <xc.h>
-#include "Fuses.h"
+#include <xc.h> //Library to Microchip Microcontrollers. 
+#include "Fuses.h"//Library to set fuses of PIC. 
+
 
 void main(void) {
 
