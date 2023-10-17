7ecee11dea097ff0db80fc1cde63a10a1b2f9f6f
Alejandro Reyes Leon <alle262001@gmail.com>
2023-10-17 00:11:09 -0500

    Update files and set inputs and outputs.

--- Disp7_Class7E1.1.X/main.c
+++ Disp7_Class7E1.1.X/main.c
@@ -14,11 +14,26 @@
 #include <xc.h> //Library to Microchip Microcontrollers. 
 #include "Fuses.h"//Library to set fuses of PIC. 
 
+//Prototype functions.
+void Configuration(void);
+
+//Main function. 
 
 void main(void) {
 
     while (1) {
 
     }
+
+}
+
+void Configuration(void) {
+    
+    OSCCON = 0x72; //Set internal oscillator. 
+    ADCON1 = 0x0F; //Set ports as digital. 
+    
+    TRISB = 0x03; //Set pin 0 and 1 as input. 
+    TRISD = 0x00; //Set D port as output. 
+    
 
 }
