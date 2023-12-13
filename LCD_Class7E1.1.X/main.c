f3a3f5981fd1c66de3d363001fa1709ea37938f6
Daniel ALejandro Reyes León <alle262001@gmail.com>
2023-12-13 04:45:05 -0500

    Create the project about LCD.

---
+++ LCD_Class7E1.1.X/main.c
@@ -0,0 +1,20 @@
+/*
+ * File:   main.c
+ * Author: Alejo
+ *
+ * Created on December 13, 2023, 4:43 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) { 
+    
+    while (1){
+        
+        
+        
+    }
+    
+    return;
+}
