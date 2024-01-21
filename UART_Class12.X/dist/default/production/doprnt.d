ac23385b07b6316dab1ea4867e46ea46b4511e61
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-01-21 00:00:12 -0500

    Delete UART project.

--- UART_Class12.X/dist/default/production/doprnt.d
+++
@@ -1,1 +0,0 @@
- dist/default/production/doprnt.d  dist/default/production/doprnt.p1:  C:/Program\ Files/Microchip/xc8/v2.41/pic/sources/c90/common/doprnt.c C:/Program\ Files/Microchip/MPLABX/v6.10/packs/Microchip/PIC18Fxxxx_DFP/1.4.151/xc8/pic/include/language_support.h
