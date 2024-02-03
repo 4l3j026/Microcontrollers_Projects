903539ec17a87e0c3d923fe0a4953cd2302ede0a
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-02 23:58:59 -0500

    Update files and finish Configurations functions.

--- RX_PIC18F46K22.X/build/default/production/main.i
+++ RX_PIC18F46K22.X/build/default/production/main.i
@@ -9813,4 +9813,39 @@ void Configurations (void){
     ANSELC = 0x00;
     ANSELD = 0x00;
 
+    TRISD = 0x00;
+
+
+    INTCONbits.GIE = 1;
+    INTCONbits.PEIE = 1;
+    RCONbits.IPEN = 0;
+
+    PIE1bits.RC1IE = 1;
+    PIR1bits.RC1IF = 0;
+
+
+    SPBRG = 103;
+    TRISCbits.RC6 = 1;
+    TRISCbits.RC7 = 1;
+
+
+    TXSTA1bits.TX91 = 0;
+    TXSTA1bits.TXEN1 = 1;
+    TXSTA1bits.SYNC1 = 0;
+    TXSTA1bits.BRGH1 = 1;
+
+
+    RCSTAbits.SPEN1 = 1;
+    RCSTAbits.RX91 = 0;
+    RCSTAbits.CREN1 = 1;
+
+
+    BAUDCON1bits.BRG16 = 0;
+# 77 "main.c"
+}
+
+void __attribute__((picinterrupt(("")))) RX_EUSART (void){
+
+
+
 }
--- RX_PIC18F46K22.X/main.c
+++ RX_PIC18F46K22.X/main.c
@@ -35,5 +35,49 @@ void Configurations (void){
     //Set pins as digital. 
     ANSELC = 0x00; 
     ANSELD = 0x00; 
+    //Set pins as outputs. 
+    TRISD = 0x00; 
+    
+    //---- Interrupts Configurations ----
+    INTCONbits.GIE = 1; //Global Interrupt Enabled. 
+    INTCONbits.PEIE = 1;  //Peripheral Interrupt Enabled. 
+    RCONbits.IPEN = 0; //Interrupt Priority disabled. 
+    
+    PIE1bits.RC1IE = 1; //Receive Enabled. 
+    PIR1bits.RC1IF = 0; //Receive Flag cleared. 
+    
+    //---- EUSART Configurations ----
+    SPBRG = 103; //Configuration 9600 bauds. 
+    TRISCbits.RC6 = 1; //Enable pin RC6 (TX1).
+    TRISCbits.RC7 = 1; //Enable pin RC7 (RX1).
+
+    //---- Transmitter configuration TXSTA1 (TX). 
+    TXSTA1bits.TX91 = 0; //9 bits transmitter disabled. 
+    TXSTA1bits.TXEN1 = 1; //Transmit Enabled. 
+    TXSTA1bits.SYNC1 = 0; //Asynchronous mode. 
+    TXSTA1bits.BRGH1 = 1; //High baud rate. 
+
+    //---- Receive configuration RCSTA1 register (RX).
+    RCSTAbits.SPEN1 = 1; //Serial port enable bit. 
+    RCSTAbits.RX91 = 0; //9 bits transmitter disabled. 
+    RCSTAbits.CREN1 = 1; //Receive continuous enabled. 
+
+    //---- Baud configuration BAUDCON1. 
+    BAUDCON1bits.BRG16 = 0; //16 bits mode disabled. 
+
+    /*
+     
+     Remember data buffers. 
+     * 
+        RCREG1; ----> Receive buffer.
+        TXREG1; ----> Transmitter buffer.
+      
+     */
+    
+}
+
+void __interrupt () RX_EUSART (void){
+    
+    
     
 }
