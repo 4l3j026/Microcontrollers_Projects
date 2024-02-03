67c5650b829f31707e1c1fa74ce5d41fa26e22cc
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-02 23:38:37 -0500

    Update files and set template of the project.

--- RX_PIC18F46K22.X/main.c
+++ RX_PIC18F46K22.X/main.c
@@ -7,7 +7,33 @@
 
 
 #include <xc.h>
+#include <string.h>
+#include "Fuses_Configuration.h"
+
+//Prototype functions. 
+void Configurations (void); 
+
+//Variables. 
 
 void main(void) {
-    return;
+    
+    //Call functions. 
+    Configurations(); 
+    
+    while (1){
+        
+        
+        
+    }
+
+}
+
+void Configurations (void){
+    
+    OSCCON = 0x72; //Set internal oscillator. 
+    
+    //Set pins as digital. 
+    ANSELC = 0x00; 
+    ANSELD = 0x00; 
+    
 }
